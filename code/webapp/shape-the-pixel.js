e104fec002f2eecbddd77efe7799ee20edcf7298
Hugo Gomes <mail@hugodaniel.pt>
2021-04-16 16:08:14 +0100

    Add dist

---
+++ code/webapp/shape-the-pixel.js
@@ -0,0 +1,10 @@
+import { ViewState } from './state/view-state.js'
+
+window.onload = () => {
+  // const viewState = new ViewState(window.innerWidth, window.innerHeight)
+  // const view = new View()
+
+  // window.location.hash = viewState.actorId
+  // view.initialize(viewState)
+  console.log('READY')
+}
